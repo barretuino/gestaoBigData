c4dff2cc20c80cc9cc2046e016b8da51135e2c1b
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2022-04-12 19:05:25 -0300

    Add files via upload

--- Aula 8/ConnectionFactory.java
+++ Aula 8/ConnectionFactory.java
@@ -1,5 +1,3 @@
-package br.com.anhanguera.jdbc;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,10 +14,19 @@ public class ConnectionFactory {
 			Class.forName("com.mysql.jdbc.Driver");
 			return
 				DriverManager.getConnection(
-					"jdbc:mysql://localhost/unisal",
+					"jdbc:mysql://localhost/test",
 					"paulo","12345679");
 		}catch(ClassNotFoundException erro){
 			throw new SQLException(erro.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			ConnectionFactory.getConnection();
+			System.out.println("Conexão Realizada com Sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
